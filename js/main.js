032cd48e1226f254289ce04150651546ce6c5067
kenbugless <kenbugless@gmail.com>
2023-11-26 10:32:45 +0900

    시간단위로 쿠키생성 코드 개선

--- js/main.js
+++ js/main.js
@@ -1,31 +1,38 @@
-/*
-  cookie는 사용자의 컴퓨터에 물리적인 파일 형태로 남기는 경량의 텍스트 자료
-  name=value 형식으로 저장됨;
-  쿠키 생성시 쿠기의 생성주기를 설정가능
-  name=value; path=/; expires=만료시간;
-  문자열.indexOf(찾을 문자열) : 전체 문자열에서 해당 문자열의 위치순서값 반환
-  전체문자열에서 찾을 문자열이 없으면 -1을 반환
-*/
 const [open, view, del] = document.querySelectorAll('nav button');
 const popup = document.querySelector('aside');
 const ck = popup.querySelector('#ck');
 const close = popup.querySelector('button');
 
+if (document.cookie.indexOf('today=done') < 0) {
+	popup.style.display = 'block';
+} else {
+	popup.style.display = 'none';
+}
+
 //쿠키 확인
 view.addEventListener('click', () => console.log(document.cookie));
+
+del.addEventListener('click', () => {
+	setCookie('today', 'done', 0);
+	alert('쿠키 삭제');
+});
+
 //팝업 열기
 open.addEventListener('click', () => {
 	popup.style.display = 'block';
 });
+
 //팝업 닫기
 close.addEventListener('click', () => {
-	if (ck.checked) setCookie('today', 'done', 1);
+	if (ck.checked) setCookie('today', 'done', 24);
 	popup.style.display = 'none';
 });
-//쿠키 생성함수
+
+//쿠키생성 함수
 function setCookie(name, value, expires) {
-	let today = new Date();
-	let duedate = today.getDate() + expires;
-	today.setDate(duedate);
-	document.cookie = `${name}=${value}; path=/; expires=${today.toString()}`;
+	let now = new Date();
+	//현재 밀리세컨드 시간값에 1시간(1000*60*60) * expires
+	let duedate = now.getTime() + 1000 * 60 * 60 * expires;
+	now.setTime(duedate);
+	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
 }
